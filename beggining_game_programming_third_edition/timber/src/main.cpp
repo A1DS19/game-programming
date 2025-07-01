5bd8f8316347a919ef566cbadfbb38e0b89ca011
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2025-07-01 14:53:20 -0700

    load and display background sprite

--- beggining_game_programming_third_edition/timber/src/main.cpp
+++ beggining_game_programming_third_edition/timber/src/main.cpp
@@ -1,8 +1,34 @@
-#include "main.hpp"
-
-void hello() { std::cout << "Hello, World!" << std::endl; }
+#include "SFML/Graphics.hpp"
 
 int main() {
-  hello();
-  return EXIT_SUCCESS;
+  sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Timber");
+
+  window.setVerticalSyncEnabled(true);
+
+  sf::Texture texture("../assets/graphics/background.png", false,
+                      sf::IntRect({0, 0},      // position
+                                  {1920, 1080} // size
+                                  ));
+  sf::Sprite sprite(texture);
+
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      } else if (const auto *keyPressed =
+                     event->getIf<sf::Event::KeyPressed>()) {
+        if (keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
+          window.close();
+        }
+      }
+    }
+
+    window.clear(sf::Color::Black);
+
+    // draw sprites here
+    window.draw(sprite);
+
+    window.display();
+  }
+  return 0;
 }
