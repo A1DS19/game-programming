75a37f301b9385a040d3a781ff2e88bc08ebe966
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2025-07-02 08:07:06 -0700

    improved flow

--- beggining_game_programming_third_edition/timber/src/main.cpp
+++ beggining_game_programming_third_edition/timber/src/main.cpp
@@ -1,26 +1,44 @@
 #include "SFML/Graphics.hpp"
+#include <iostream>
+
+void eventHandler(const sf::Event &event, sf::RenderWindow &window) {
+  if (event.is<sf::Event::Closed>()) {
+    window.close();
+  }
+}
+
+void keyPressedHandler(const sf::Event &event, sf::RenderWindow &window) {
+  if (const auto *keyPressed = event.getIf<sf::Event::KeyPressed>()) {
+    if (keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
+      window.close();
+    }
+  }
+}
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Timber");
+  constexpr auto screenDims = sf::Vector2u{1920, 1080};
+  sf::RenderWindow window(sf::VideoMode(screenDims), "Timber");
 
   window.setVerticalSyncEnabled(true);
 
-  sf::Texture texture("../assets/graphics/background.png", false,
-                      sf::IntRect({0, 0},      // position
-                                  {1920, 1080} // size
-                                  ));
-  sf::Sprite sprite(texture);
+  sf::Texture textureBackground;
+  if (!textureBackground.loadFromFile(
+          "../assets/graphics/background.png", false,
+          sf::IntRect({0, 0},                                   // position
+                      static_cast<sf::Vector2<int>>(screenDims) // size
+                      ))) {
+    std::cerr << "could not load texture" << std::endl;
+    return 1;
+  }
+
+  sf::Sprite sprite(textureBackground);
 
   while (window.isOpen()) {
-    while (const std::optional event = window.pollEvent()) {
-      if (event->is<sf::Event::Closed>()) {
-        window.close();
-      } else if (const auto *keyPressed =
-                     event->getIf<sf::Event::KeyPressed>()) {
-        if (keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
-          window.close();
-        }
-      }
+    while (const std::optional maybeEvent = window.pollEvent()) {
+      const sf::Event &event = *maybeEvent;
+
+      eventHandler(event, window);
+      keyPressedHandler(event, window);
     }
 
     window.clear(sf::Color::Black);
